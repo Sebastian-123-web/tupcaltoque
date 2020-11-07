63cd659dcdb7a9ff9d82c524c29b73fcbf3a59bc
Sebastian-123-web <sebastian09_26@hotmail.com>
2020-11-07 14:04:55 -0500

    .

--- php/sesion.php
+++ php/sesion.php
@@ -9,15 +9,10 @@
 
     function sesionAutentificacion(){
       include 'conexion.php';
-      $sql = "SELECT * FROM `usuario` WHERE `id_user`=\'$this->usuario\' AND `contraseña`=\'$this->contraseña\'";
-      //$sql = 'SELECT `tipo_user` FROM `usuario` WHERE `id_user`="'.$this->usuario.'" AND `contraseña`="'.$this->contraseña.'"';
+      $sql = 'SELECT * FROM `usuario` WHERE `id_user`="rbanagasta" AND `contraseña`="RB2020"';
       $query = mysqli_query($link, $sql);
+      var_dump($query);exit;
       $row = mysqli_fetch_array($query);
-      if(!isset($row[0])) {
-        return 'Error';
-      }else{
-        return $cuenta = true;
-      }
     }
     function sesionIniciada(){
 
