f54ac83c815af23fbe05638ae04c350c3b7b15c2
Sebastian-123-web <sebastian09_26@hotmail.com>
2020-11-08 03:23:06 -0500

    .

--- php/sesion.php
+++ php/sesion.php
@@ -11,8 +11,17 @@
       include 'conexion.php';
       $sql = 'SELECT * FROM `usuario` WHERE `id_user`="rbanagasta" AND `contraseña`="RB2020"';
       $query = mysqli_query($link, $sql);
-      var_dump($query);exit;
+      //var_dump($query);exit;
       $row = mysqli_fetch_array($query);
+      session_start();
+      //var_dump($row);
+      $_SESSION["usuario"]["id_user"] = $row["id_user"];
+      $_SESSION["usuario"]["contraseña"] = $row["contraseña"];
+      $_SESSION["usuario"]["nombre"] = $row["nombre"];
+      $_SESSION["usuario"]["apellido"] = $row["apellido"];
+      $_SESSION["usuario"]["correo"] = $row["correo"];
+      $_SESSION["usuario"]["tipo_user"] = $row["tipo_user"];
+
     }
     function sesionIniciada(){
 
